8a6e1b6cfdcebc26ba04b1251fd7e67a164f2e2f
Hachen-W <mr.laiker.ru@gmail.com>
2026-04-04 03:03:13 +0300

    solve task3 + testing

---
+++ lab02/OOP/task3/script.js
@@ -0,0 +1,33 @@
+class User {
+    #tel = null;
+
+    constructor(age, name, tel) {
+        this.age = age;
+        this.name = name;
+        this.tel = tel;
+    }
+
+    get tel() {
+        return this.#tel;
+    }
+
+    set tel(value) {
+        const regex = /^\+7\d{10}$/;
+
+        if (regex.test(value)) {
+            this.#tel = value;
+        } else {
+            console.log("Некорректный номер телефона");
+        }
+    }
+
+    hello() {
+        console.log(`Hi! My name is ${this.name}.\n` + 
+            `And I am ${this.age} years old.\n` +
+            `My phone number is ${this.tel}\n`);
+    }
+}
+
+
+const user1 = new User(25, "Alex", "+71234567890");
+user1.hello();
